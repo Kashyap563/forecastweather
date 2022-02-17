020dfa8c0dbcaa7040d65f2e927631b1579c6094
Kashyap Agarwal <kashyapagarwal563@gmail.com>
2022-02-17 12:24:48 +0530

    first

---
+++ index.js
@@ -0,0 +1,34 @@
+const http=require('http');
+const fs=require('fs');
+var requests=require("requests");
+
+const homeFile = fs.readFileSync("home.html","utf-8");
+const replaceVal=(tempVal,orgVal)=>{
+    let temperature=tempVal.replace("{%tempval%}",orgVal.main.temp);
+     temperature=temperature.replace("{%tempmin%}",orgVal.main.temp_min);
+     temperature=temperature.replace("{%tempmax%}",orgVal.main.temp_max);
+     temperature=temperature.replace("{%location%}",orgVal.name);
+     temperature=temperature.replace("{%country%}",orgVal.sys.country);
+     temperature=temperature.replace("{%tempstatus%}",orgVal.weather[0].main);
+     return temperature;
+}
+const server = http.createServer((req,res)=>{
+   if (req.url=="/"){
+      requests("https://api.openweathermap.org/data/2.5/weather?q=London&appid=1469f48b83180af410b12ac881642d1f")
+      .on("data",function(chunk){
+          const objdata=JSON.parse(chunk);
+          const arrData=[objdata
+          ];
+         // console.log(arrData[0].main.temp);
+         const realTimeData=arrData.map((val)=>replaceVal(homeFile,val)).join('');
+         res.write(realTimeData);
+      })
+      .on("end",function(err){
+          if(err)return console.log("connection closed due to error",err);
+          console.log("end");
+          res.end();
+      });
+   }
+});
+
+server.listen(8000,"127.0.0.1");
